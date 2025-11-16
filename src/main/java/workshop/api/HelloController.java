69ef5fc503c57ed97c950c459b6da8725be4dc65
Elias <520268@student.fontys.nl>
2025-11-16 02:01:38 +0100

    third

--- src/main/java/workshop/api/HelloController.java
+++ src/main/java/workshop/api/HelloController.java
@@ -1,3 +1,16 @@
+package workshop.api;
+
+import workshop.BuildInfoApplication;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.management.ManagementFactory;
+import java.time.Duration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
